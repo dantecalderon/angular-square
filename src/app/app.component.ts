fc033255a9691fe9b88a28373e2915a8b92d0884
dantehemerson <dantehemerson@gmail.com>
2018-10-29 23:23:07 -0500

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularSquare'
   lugares:any = [
-    {active: true, nombre: 'Floreria la gardenia'},
-    {active: false, nombre: 'Donas la pasadita'},
-    {active: true, nombre: 'Veterinaria'},
-    {active: true, nombre: 'Alfabetería'},
-    {active: false, nombre: 'YOutube'},
-    {active: true, nombre: 'otra cosa'}
+    {cercania: 1, distancia: 1, active: true, nombre: 'Floreria la gardenia'},
+    {cercania: 1, distancia: 1.8, active: false, nombre: 'Donas la pasadita'},
+    {cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria'},
+    {cercania: 3, distancia: 10, active: true, nombre: 'Alfabetería'},
+    {cercania: 3, distancia: 35, active: false, nombre: 'YOutube'},
+    {cercania: 3, distancia: 120, active: true, nombre: 'otra cosa'}
   ];
   lat: number = 4.6560663;
   lng: number = -74.0595918;
